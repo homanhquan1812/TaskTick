8bcef957255c4f0337d977f85fed97368a31b3d1
Hồ Mạnh Quân <homanhquan1812@gmail.com>
2024-09-20 18:15:29 +0700

    Update Headers.jsx

--- frontend/src/components/Headers.jsx
+++ frontend/src/components/Headers.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import jwtDecode from "jwt-decode"
+import { jwtDecode } from "jwt-decode"
 
 const Headers = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false)
