701934ff6d55fd6e93cf1c91c543f54b25e14514
Hồ Mạnh Quân <homanhquan1812@gmail.com>
2024-09-20 18:13:54 +0700

    Update Headers.jsx

--- frontend/src/components/Headers.jsx
+++ frontend/src/components/Headers.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { jwtDecode } from "jwt-decode"
+import { Link } from 'react-router-dom'
+import jwtDecode from "jwt-decode"
 
 const Headers = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false)
@@ -55,53 +56,41 @@ const Headers = () => {
         <header>
             <nav className="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
                 <div className="container">
-                <a className="navbar-brand" href="/"><img style={{height: '47.5px'}} src='/img/image2.png'></img></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon" />
-                </button>
-                <div className="navbar-collapse collapse d-sm-inline-flex justify-content-between">
-                    <ul className="navbar-nav flex-grow-1">
-                    <li className="nav-item">
-                        <a className="nav-link text-dark" href="/">Trang chủ</a>
-                    </li>     
-                    {
-                        isLoggedIn ? (
-                            <>
-                                <li className="nav-item">
-                                    <a className="nav-link text-dark" href={'/dailytask'}>Task hằng ngày</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link text-dark" href={`/info/${userId}`}>Thông tin cá nhân</a>
-                                </li>
-                                {
-                                    /*
-                                    <li className="nav-item">
-                                    <a className="nav-link text-dark" href={`/info/${userId}`}>Task khác</a>
-                                </li>  
-                                     */
-                                }
-                                <li className="nav-item">
-                                    <a className="nav-link text-dark" onClick={handleLogout} href="/">Đăng xuất</a>
-                                </li> 
-                            </>
-                        ) : (
-                            <>
-                                <li className="nav-item">
-                                    <a className="nav-link text-dark" href={'/login'}>Đăng nhập</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link text-dark" href={'/register'}>Đăng ký</a>
-                                </li>
-                            </>
-                        )
-                    }
-                    
-                                {/* 
-                                <li className="nav-item">
-                                    <a className="nav-link text-dark" onClick={handleLogout} href="/">Đăng xuất</a>
-                                </li> */}        
-                    </ul>
-                </div>
+                    <Link className="navbar-brand" to="/"><img style={{height: '47.5px'}} src='/img/image2.png' alt="Logo"></img></Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="navbar-collapse collapse d-sm-inline-flex justify-content-between">
+                        <ul className="navbar-nav flex-grow-1">
+                            <li className="nav-item">
+                                <Link className="nav-link text-dark" to="/">Trang chủ</Link>
+                            </li>     
+                            {
+                                isLoggedIn ? (
+                                    <>
+                                        <li className="nav-item">
+                                            <Link className="nav-link text-dark" to='/dailytask'>Task hằng ngày</Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link className="nav-link text-dark" to={`/info/${userId}`}>Thông tin cá nhân</Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link className="nav-link text-dark" onClick={handleLogout} to="/">Đăng xuất</Link>
+                                        </li> 
+                                    </>
+                                ) : (
+                                    <>
+                                        <li className="nav-item">
+                                            <Link className="nav-link text-dark" to='/login'>Đăng nhập</Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link className="nav-link text-dark" to='/register'>Đăng ký</Link>
+                                        </li>
+                                    </>
+                                )
+                            }
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </header>
